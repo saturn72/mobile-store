62c1a7480fe93ce97a6ee17877f7ea93e2b57098
roi <roi@saturn72.com>
2020-10-31 21:52:20 +0200

    keep working on product card

--- components/VendorStore.vue
+++ components/VendorStore.vue
@@ -1,98 +1,131 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col
-      cols="12"
-      v-for="product in catalog.products"
-      v-bind:key="product.id"
-    >
-      <v-card class="mx-auto" max-width="344">
-        <v-row>
-          <v-col cols="3">
-            <v-avatar class="profile" color="grey" size="100">
-              <v-img :src="getPrimaryMediaItem(product.media.images)">
-                <template v-slot:placeholder>
-                  <v-row
-                    class="fill-height ma-0"
-                    align="center"
-                    justify="center"
-                  >
-                    <v-progress-circular
-                      indeterminate
-                      color="black"
-                    ></v-progress-circular>
-                  </v-row>
-                </template>
-              </v-img>
-            </v-avatar>
-          </v-col>
-          <v-col>
-            <v-card-title> {{ product.name }} </v-card-title>
-            <v-card-subtitle> {{ product.shortDescription }} </v-card-subtitle>
-          </v-col>
-        </v-row>
-        <v-card-actions>
-          <v-btn color="green lighten-2" text block @click="show = !show">
-            <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-            <v-spacer></v-spacer>
-            Explore
-          </v-btn>
-        </v-card-actions>
-        <v-expand-transition>
-          <div v-show="show">
-            <v-carousel hide-delimiters>
-              <v-carousel-item
-                ripple
-                v-for="(item, i) in toCarouselData(product.media)"
-                :key="i"
-                :src="item.src"
-                :title="item.description"
-              >
-              </v-carousel-item>
-            </v-carousel>
-            <v-divider />
-
-            <v-img
-              src="https://www.zooplus.co.uk/magazine/wp-content/uploads/2018/01/Female-Dogs-in-Heat-768x512.jpeg"
-              height="200px"
-            >
-              <template v-slot:placeholder>
-                <v-row class="fill-height ma-0" align="center" justify="center">
-                  <v-progress-circular
-                    indeterminate
-                    color="black"
-                  ></v-progress-circular>
+  <div>
+    <VendorStoreHeader />
+    <v-row justify="center" align="center">
+      <v-col
+        cols="12"
+        v-for="product in catalog.products"
+        v-bind:key="product.id"
+      >
+        <v-card class="mx-auto" max-width="344">
+          <v-row>
+            <v-col cols="3">
+              <v-avatar class="profile" color="grey" size="100">
+                <v-img :src="getPrimaryMediaItem(product.media)">
+                  <template v-slot:placeholder>
+                    <v-row
+                      class="fill-height ma-0"
+                      align="center"
+                      justify="center"
+                    >
+                      <v-progress-circular
+                        indeterminate
+                        color="black"
+                      ></v-progress-circular>
+                    </v-row>
+                  </template>
+                </v-img>
+              </v-avatar>
+            </v-col>
+            <v-col>
+              <v-card-title> {{ product.name }} </v-card-title>
+              <v-card-subtitle>
+                {{ product.shortDescription }}
+              </v-card-subtitle>
+              <v-card-subtitle>
+                <v-row>
+                  <v-col cols="3">
+                    <strong>{{ product.price }} ₪</strong>
+                  </v-col>
+                  <v-col cols="9">
+                    <v-row>
+                      <v-btn fab x-small color="primary">
+                        <v-icon dark> mdi-plus </v-icon>
+                      </v-btn>
+                      <v-spacer />
+                      <v-chip active class="ma-2" small outlined pill>
+                        10<v-icon left>mdi-cart-outline</v-icon>
+                      </v-chip>
+                      <v-spacer />
+                      <v-btn fab x-small color="primary">
+                        <v-icon dark> mdi-minus </v-icon>
+                      </v-btn>
+                    </v-row>
+                  </v-col>
                 </v-row>
-              </template>
-            </v-img>
-            <v-divider></v-divider>
+              </v-card-subtitle>
+            </v-col>
+          </v-row>
+          <v-card-actions>
+            <v-btn color="green lighten-2" text block @click="show = !show">
+              <v-icon>{{
+                show ? 'mdi-chevron-up' : 'mdi-chevron-down'
+              }}</v-icon>
+              <v-spacer></v-spacer>
+              Explore
+            </v-btn>
+          </v-card-actions>
+          <v-expand-transition>
+            <div v-show="show">
+              <v-carousel hide-delimiters height="auto">
+                <v-carousel-item
+                  ripple
+                  v-for="(item, i) in mediaToCarouselItems(product.media)"
+                  :key="i"
+                  :title="item.description"
+                >
+                  <v-img :src="item.src">
+                    <template v-slot:placeholder>
+                      <v-row
+                        class="fill-height ma-0"
+                        align="center"
+                        justify="center"
+                      >
+                        <v-progress-circular
+                          indeterminate
+                          color="green"
+                        ></v-progress-circular>
+                      </v-row>
+                    </template>
+                  </v-img>
+                </v-carousel-item>
+              </v-carousel>
+              <v-divider />
 
-            <v-card-text>
-              I'm a thing. But, like most politicians, he promised more than he
-              could deliver. You won't have time for sleeping, soldier, not with
-              all the bed making you'll be doing. Then we'll go with that data
-              file! Hey, you add a one and two zeros to that or we walk! You're
-              going to do his laundry? I've got to find a way to escape.
-            </v-card-text>
-          </div>
-        </v-expand-transition>
-      </v-card>
-    </v-col>
-  </v-row>
+              <v-card-text>
+                I'm a thing. But, like most politicians, he promised more than
+                he could deliver. You won't have time for sleeping, soldier, not
+                with all the bed making you'll be doing. Then we'll go with that
+                data file! Hey, you add a one and two zeros to that or we walk!
+                You're going to do his laundry? I've got to find a way to
+                escape.
+              </v-card-text>
+            </div>
+          </v-expand-transition>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 <script>
-const imageTypes = {
+const mediaTypes = {
   image: 'img',
   video: 'video',
 }
+import VendorStoreHeader from './VendorStoreHeader'
 export default {
   methods: {
     getPrimaryMediaItem: function (items) {
-      return items.find((i) => i.isPrimary) ?? items[0]
+      return (
+        items.images.find((i) => i.isPrimary) ??
+        items.videos.find((i) => i.isPrimary) ??
+        items[0]
+      )
     },
-    toCarouselData: function (media) {
+    mediaToCarouselItems: function (media) {
       const imgs = media.images.map((i) => {
         return {
-          type: imageTypes.image,
+          type: mediaTypes.image,
           src: i.src,
           description: i.description,
           displayOrder: i.displayOrder,
@@ -100,28 +133,31 @@ export default {
         }
       })
       let cm = [...imgs]
-
       const videos = media.videos.map((i) => {
         return {
-          type: imageTypes.video,
+          type: mediaTypes.video,
           src: i.src,
           description: i.description,
           displayOrder: i.displayOrder,
           isPrimary: i.isPrimary,
         }
       })
-      cm = [...videos]
+      // cm = [...videos]
       return cm
     },
   },
   data() {
     return {
       show: false,
+      consts: {
+        mediaTypes,
+      },
       catalog: {
         products: [
           {
             id: '1',
             name: 'product_1',
+            price: 1.11,
             shortDescription: 'this is the short description',
             fullDescription:
               'this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, ',
@@ -161,6 +197,7 @@ export default {
           {
             id: '234',
             name: 'product_1',
+            price: 1.22,
             shortDescription: 'this is the short description',
             fullDescription:
               'this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, ',
@@ -198,6 +235,7 @@ export default {
           {
             id: '2',
             name: 'product_1',
+            price: 1.22,
             shortDescription: 'this is the short description',
             fullDescription:
               'this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, ',
@@ -235,6 +273,7 @@ export default {
           {
             id: 'dfg654dsf',
             name: 'product_1',
+            price: 1.22,
             shortDescription: 'this is the short description',
             fullDescription:
               'this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, ',
@@ -272,6 +311,7 @@ export default {
           {
             id: 'sdgsdfb',
             name: 'product_1',
+            price: 1.22,
             shortDescription: 'this is the short description',
             fullDescription:
               'this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, ',
@@ -309,6 +349,7 @@ export default {
           {
             id: '3',
             name: 'product_1',
+            price: 1.22,
             shortDescription: 'this is the short description',
             fullDescription:
               'this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, ',
@@ -346,6 +387,7 @@ export default {
           {
             id: 'gsdfg',
             name: 'product_1',
+            price: 1.22,
             shortDescription: 'this is the short description',
             fullDescription:
               'this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, ',
@@ -383,6 +425,7 @@ export default {
           {
             id: 'dfg',
             name: 'product_1',
+            price: 1.22,
             shortDescription: 'this is the short description',
             fullDescription:
               'this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, ',
@@ -420,6 +463,7 @@ export default {
           {
             id: 'sdf',
             name: 'product_1',
+            price: 1.22,
             shortDescription: 'this is the short description',
             fullDescription:
               'this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, this is the full description, ',
---
+++ components/VendorStoreHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-sheet height="auto">
+    <v-container class="fill-height">
+      <v-row align="center" justify="center">
+        <h1 class="text-center display-3">{{ storeTitle }}</h1>
+      </v-row>
+    </v-container>
+  </v-sheet>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      storeTitle: 'This is vendor store header',
+    }
+  },
+}
+</script>
