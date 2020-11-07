625d190483c63bb8aed553c7f46ffde610fc04cb
roi <roi@saturn72.com>
2020-11-07 22:54:10 +0200

    add whatsapp closes #14

--- components/SpeedDial.vue
+++ components/SpeedDial.vue
@@ -20,28 +20,46 @@
       </v-btn>
     </template>
     <v-btn
+      v-if="!isCheckoutPage()"
       fab
       small
       color="primary"
       :disabled="cartTotal() === 0"
-      @click="toCheckout()"
+      @click="checkout()"
     >
       <v-icon>mdi-cart-check</v-icon>
     </v-btn>
-    <v-btn fab dark small color="green">
+    <v-btn fab dark small color="orange" @click="phone()">
+      <v-icon>mdi-phone</v-icon>
+    </v-btn>
+    <v-btn fab dark small color="green" @click="whatsapp()">
       <v-icon>mdi-whatsapp</v-icon>
     </v-btn>
   </v-speed-dial>
 </template>
 <script>
+const CheckoutPath = 'checkout'
 import { mapGetters } from 'vuex'
 export default {
   computed: {
     ...mapGetters('cart', ['cartTotal']),
   },
   methods: {
-    toCheckout: function () {
+    isCheckoutPage() {
+      const idx = $nuxt.$route.path.toLowerCase().indexOf(CheckoutPath)
+      return idx === 0 || idx === 1
+    },
+    checkout: function () {
       this.$router.push('/checkout')
+    },
+    phone() {
+      this.redirectTo('tel:+972542204119')
+    },
+    whatsapp() {
+      this.redirectTo('https://wa.me/972542204119?text=יש%20לי%20שאלה:%0D%0A')
+    },
+    redirectTo(url) {
+      window.location.replace(url)
     },
   },
   data() {
