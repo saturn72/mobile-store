c5e3e9098ccb3aff297688f613cc0a73c941d9ea
roi <roi@saturn72.com>
2020-11-16 20:50:00 +0200

    update product qty  from CartSummary

--- components/CartSummary.vue
+++ components/CartSummary.vue
@@ -1,41 +1,55 @@
 <template>
-  <v-simple-table fixed-header dense>
-    <template>
-      <thead>
-        <tr>
-          <th class="text-left">Name</th>
-          <th class="text-left">Price</th>
-          <th class="text-left">Quantity</th>
-          <th class="text-left">Item Total</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="ci in getCart().cartItems" :key="ci.product.id" flex>
-          <td>
-            <v-row
-              ><ProductAvatar :product="ci.product" v-bind:size="40"
-            /></v-row>
-            <v-row>{{ ci.product.name }}</v-row>
-          </td>
-          <td>{{ ci.product.price }}</td>
-          <td>{{ ci.quantity }}</td>
-          <td>{{ (ci.product.price * ci.quantity).toFixed(2) }}</td>
-        </tr>
-        <tr>
-          <td>Cart Total</td>
-          <td></td>
-          <td></td>
-          <td>{{ getCartTotal().toFixed(2) }}</td>
-        </tr>
-      </tbody>
-    </template>
-  </v-simple-table>
+  <v-sheet>
+    <v-simple-table fixed-header dense>
+      <template>
+        <thead>
+          <tr>
+            <th class="text-right">Name</th>
+            <th class="text-right">Quantity</th>
+            <th class="text-right">Prices</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="ci in getCart().cartItems" :key="ci.product.id" flex>
+            <td>
+              <v-row>
+                <ProductAvatar :product="ci.product" v-bind:size="40" />
+              </v-row>
+              <v-row>{{ ci.product.name }}</v-row>
+            </td>
+            <td><AddSubtractProduct :product="ci.product" icon="" /></td>
+            <td>
+              <v-row>
+                <small>
+                  Unit: <strong>{{ ci.product.price }} </strong></small
+                ></v-row
+              >
+              <v-row>
+                <small>Item Total: </small
+                >{{ (ci.product.price * ci.quantity).toFixed(2) }}
+              </v-row>
+            </td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+    <v-divider />
+    <v-row align="center" justify="space-around">
+      <v-col> <h3>Cart Total:</h3> </v-col>
+      <v-spacer />
+      <v-col>
+        <v-chip>{{ getCartTotal().toFixed(2) }} </v-chip>
+      </v-col>
+    </v-row>
+  </v-sheet>
 </template>
-<script>
+<script lang="ts">
 import { mapGetters } from 'vuex'
-export default {
+
+import Vue from 'vue'
+export default Vue.extend({
   computed: {
     ...mapGetters('cart', ['getCart', 'getCartTotal']),
   },
-}
+})
 </script>
